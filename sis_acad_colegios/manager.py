a104df55724a1e21e949a51b1e26c204c0101fd1
waen <waen@iMac-de-wescobar.local>
2022-04-18 23:00:53 -0500

    added DocentesManager for Docentes

---
+++ sis_acad_colegios/manager.py
@@ -0,0 +1,28 @@
+from random import vonmisesvariate
+from readline import read_init_file
+from tkinter.messagebox import NO
+from django.contrib.auth.base_user import BaseUserManager
+
+class DocentesManager(BaseUserManager):
+    user_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+
+        if not email:
+            raise ValueError('Email is require')
+
+        email = self.normalize_email(email)
+        user = self.model(email = email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(('Super user must have is_staff True'))
+        
+        return self.create_user(email, password, **extra_fields)

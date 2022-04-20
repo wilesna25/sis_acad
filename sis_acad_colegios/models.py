4ff6a8ebcc8ac4a0083293423407a9bc1bb1ac5b
waen <waen@iMac-de-wescobar.local>
2022-04-19 22:24:54 -0500

    database model v1.1 git push

--- sis_acad_colegios/models.py
+++ sis_acad_colegios/models.py
@@ -1,4 +1,7 @@
+from operator import mod
+from statistics import mode
 from tabnanny import verbose
+from turtle import ondrag
 from django.db import models
 from django.contrib.auth.models import AbstractUser, User
 from datetime import date
@@ -13,6 +16,10 @@ class Jornadas(models.Model):
     def __str__(self):
         return self.jornada
 
+    class Meta:
+        verbose_name = "Jornada"
+        verbose_name_plural = "Jornadas"
+
 
 class Sedes(models.Model):
     nombre = models.CharField(max_length=200, null=True)
@@ -22,14 +29,22 @@ class Sedes(models.Model):
     def __str__(self):
         return self.nombre
 
+    class Meta:
+        verbose_name = "Sede"
+        verbose_name_plural = "Sedes"
+
 
 class PeriodoAcademico(models.Model):
-    periodo = models.CharField(max_length=200, null=True)
+    periodo = models.CharField(max_length=200, null=True, unique=True)
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
 
     def __str__(self):
         return self.periodo
+
+    class Meta:
+        verbose_name = "Periodo Académico"
+        verbose_name_plural = "Periodos Académicos"
 
 
 class Departamentos(models.Model):
@@ -38,6 +53,10 @@ class Departamentos(models.Model):
     def __str__(self):
         return self.departamento
 
+    class Meta:
+        verbose_name = "Departamento"
+        verbose_name_plural = "Departamentos"
+
 
 class Ciudades(models.Model):
     ciudad = models.CharField(max_length=200)
@@ -45,12 +64,9 @@ class Ciudades(models.Model):
     def __str__(self):
         return self.ciudad
 
-
-# class Niveles_academicos_docentes(models.Model):
-  #  nivel_academico = models.CharField(max_length=200, null=True)
-#
-   # def __str__(self):
-   #     return self.nivel_academico
+    class Meta:
+        verbose_name = "Ciudad"
+        verbose_name_plural = "Ciudades"
 
 
 class AreasAsignaturas(models.Model):
@@ -59,6 +75,10 @@ class AreasAsignaturas(models.Model):
 
     def __str__(self):
         return self.area
+
+    class Meta:
+        verbose_name = "Área Asignatura"
+        verbose_name_plural = "Áreas Asignaturas"
 
 
 class Asignaturas(models.Model):
@@ -69,13 +89,22 @@ class Asignaturas(models.Model):
     def __str__(self):
         return self.asignatura
 
+    class Meta:
+        verbose_name = "Asignatura"
+        verbose_name_plural = "Asignaturas"
+
 
 class GradosAcademicos(models.Model):
     grado = models.CharField(max_length=200, null=True)
     descripcion = models.CharField(max_length=500, null=True)
+    sede = models.ForeignKey(Sedes, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.grado
+
+    class Meta:
+        verbose_name = "Grado Académico"
+        verbose_name_plural = "Grados Académicos"
 
 
 class LogrosAsignaturas(models.Model):
@@ -83,19 +112,16 @@ class LogrosAsignaturas(models.Model):
     asignatura = models.ForeignKey(Asignaturas,  on_delete=models.CASCADE)
     nivel_desempeno = models.CharField(max_length=50)
     logro = models.CharField(max_length=500)
+    
+    class Meta:
+        verbose_name = "Logro Asignatura"
+        verbose_name_plural = "Logros Asignaturas"
 
 
-# class User(AbstractUser):
-#     is_teacher = models.BooleanField(default=False)
-#     is_student = models.BooleanField(default=False)
-#     is_admin = models.BooleanField(default=False)
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
 
 
 class Docentes(AbstractUser):
-    #user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
-    #username = None
+
     email = models.EmailField(unique=True)
     dni = models.IntegerField(unique=True, null=True)
     direccion = models.CharField(max_length=200, null=True)
@@ -106,20 +132,15 @@ class Docentes(AbstractUser):
     USERNAME_FIELD = 'email'
 
     def __str__(self):
+        if self.first_name and self.last_name:
+            return self.last_name + " " + self.first_name
         return self.email
 
     class Meta:
         db_table = "Docentes"
         verbose_name = "Docente"
         verbose_name_plural = "Docentes"
-    #nombres = models.CharField(max_length=200, null=True)
-    #apellidos = models.CharField(max_length=200, null=True)
-  #  telefono  = models.IntegerField(null=True)
-   # correo  = models.CharField(max_length=200, null=True)
-   # fecha_nacimiento = models.DateField(null=True)
-   # departamento = models.ForeignKey(Departamentos, on_delete=models.DO_NOTHING)
-   # ciudad = models.ForeignKey(Ciudades, on_delete=models.DO_NOTHING)
-   # nivel_academico_docente = models.ForeignKey(Niveles_academicos_docentes, on_delete=models.DO_NOTHING)
+
 
 
 class Grupos(models.Model):
@@ -132,7 +153,9 @@ class Grupos(models.Model):
     def __str__(self):
         return self.grupo
 
-# asignacion academica
+    class Meta:
+        verbose_name = "Grupo"
+        verbose_name_plural = "Grupos"
 
 
 class Clases(models.Model):
@@ -144,7 +167,11 @@ class Clases(models.Model):
     def __str__(self):
         return self.clase
 
+    class Meta:
+        verbose_name = "Clase"
+        verbose_name_plural = "Clases"
 
+    
 class Estudiantes(models.Model):
     cod_estudiante = models.CharField(max_length=10, unique=True)
     dni = models.IntegerField(unique=True, null=False, default='1')
@@ -154,13 +181,13 @@ class Estudiantes(models.Model):
     telefono = models.IntegerField(null=True)
     correo = models.CharField(max_length=200, null=True)
     fecha_nacimiento = models.DateField(null=True)
-   # nombres_acudiente = models.CharField(max_length=200, null=True)
-   # telefonos_acudiente = models.IntegerField()
-    #direccion_acudiente = models.CharField(max_length=200, null=True)
-    #eps = models.ForeignKey(Eps, on_delete=models.DO_NOTHING, null=True)
-   # departamento = models.ForeignKey(Departamentos, on_delete=models.DO_NOTHING, null=True)
-    #ciudad = models.ForeignKey(Ciudades, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return self.apellidos + " " + self.nombres
+
+    class Meta:
+        verbose_name = "Estudiante"
+        verbose_name_plural = "Estudiantes"
 
 class Matriculas(models.Model):
     estudiante = models.ForeignKey(
@@ -168,12 +195,38 @@ class Matriculas(models.Model):
     grupo = models.ForeignKey(Grupos, on_delete=models.CASCADE, null=True)
     fecha = models.DateField(default=date.today)
 
+    def __str__(self):
+        return self.grupo.grupo + " - " + self.estudiante.cod_estudiante + " ; " + self.estudiante.apellidos + " " + self.estudiante.nombres
+    
+    class Matriculas:
+        verbose_name = "Matriculas"
+        verbose_name_plural = "Matriculas"
+
 
 class Estudiantes_por_Grupo(models.Model):
     grupo = models.ForeignKey(Grupos, on_delete=models.CASCADE, null=True)
     estudiante = models.ForeignKey(
         Estudiantes, on_delete=models.CASCADE, null=True)
+    
+    class Meta:
+        verbose_name = "Estudiante por grupo"
+    verbose_name_plural = "Estudiantes por grupos"
 
+
+class ConceptosAcademicos(models.Model):
+    grado_academico = models.ForeignKey(
+        GradosAcademicos, on_delete=models.CASCADE, null=True)
+    asignaturas = models.ForeignKey(
+        Asignaturas, on_delete=models.CASCADE, null=True)
+    periodo_academico = models.ForeignKey(
+        PeriodoAcademico, on_delete=models.CASCADE, null=True)
+    nota_maxima = models.IntegerField(null=True)
+    nota_minima = models.IntegerField(null=True)
+    concepto_academico = models.CharField(null=True, max_length=500)
+    
+    class Meta:
+        verbose_name = "Concepto Académico"
+        verbose_name_plural = "Conceptos académicos"
 
 class FallasAsistencias(models.Model):
     estudiante = models.ForeignKey(
@@ -182,6 +235,10 @@ class FallasAsistencias(models.Model):
     justificada = models.BooleanField(default=False)
     observaciones = models.CharField(max_length=500, null=True)
     fecha = models.DateField(default=date.today)
+
+    class Meta:
+        verbose_name = "Falla Asistencia"
+        verbose_name_plural = "Falla Asistencias"
 
 
 class Calificaciones(models.Model):
@@ -192,3 +249,6 @@ class Calificaciones(models.Model):
     calificacion = models.FloatField(null=True)
     nivel_desempeno = models.CharField(max_length=50, null=True)
 
+    class Meta:
+        verbose_name = "Calificación"
+        verbose_name_plural = "Calificaciones"
